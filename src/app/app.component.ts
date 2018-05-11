b7bd71882999893d45a5fdcf8ca3306fba105702
Meena KUMARI <mkumari@asi.bvcorp.corp>
2018-05-11 16:08:06 +0530

    commiting in component-binding-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   templateUrl: './app.component.html'
 })
 export class AppComponent{
-  title :string = 'The Dashboard Data binding Branch 1';
+  title :string = 'The Dashboard component-binding-1';
 }
