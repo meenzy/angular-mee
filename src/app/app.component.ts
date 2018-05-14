809b1f487478b99158e9ac482449a658a66ec6e9
Meena KUMARI <mkumari@asi.bvcorp.corp>
2018-05-14 15:04:26 +0530

    removing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,4 @@ export class AppComponent{
   title :string = 'The Dashboard Data binding Branch 1';
   branch1 : string = 'branch 1 string';
   branch2 : string = 'branch 2 code';
-  code : string = '200';
 }
