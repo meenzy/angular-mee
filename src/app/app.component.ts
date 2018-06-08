33e3c6d12ed6c719836bf1ccc5a899aef5019d13
Meena KUMARI <meena.kumari@in.bureauveritas.com>
2018-06-08 17:25:57 +0530

    pushing changes to angular-data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from "@angular/core";
   templateUrl: './app.component.html'
 })
 export class AppComponent{
-  title :string = 'The Dashboard Data binding Branch 1';
-  branch1 : string = 'branch 1 string';
-  branch2 : string = 'branch 2 code';
-  code : string = '200';
-  newCode : string = '222';
-}
+  title :string = 'The Dashboard Data';
+  }
